c8e897e98a8cc016c8aee3fb0b0638494f1a6b13
chan-d <chan.danny516@gmail.com>
2016-03-17 14:06:13 -0700

    blog posts controller made and defined, added nav bar to splash page, styled sessions pages

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,54 @@
 class BlogController < ApplicationController
   before_filter :authorize
-  
+  def index
+    @blog = Blog.all.order("created_at DESC")
+  end
+
+  def new
+    @blog = Blog.new
+    render :new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:notice] = "successfully created blog post"
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = "something went wrong"
+      redirect_to blogs_path
+    end
+  end
+
+  def show
+    @blog = get_blog
+  end
+
+  def edit
+    blog = get_blog
+    blog.update_attributes(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = get_blog
+    blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+
+  def blog_id
+    params[:id]
+  end
+
+  def get_blog
+    Blog.find_by_id(blog_id)
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :post, :user_id)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,24 @@ Rails.application.routes.draw do
   get '/', to: 'pages#home'
 
   # Sessions
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as:'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
   # users
   get '/signup', to: 'users#new'
-  get '/users', to: 'users#create'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy', as:'delete_user'
+
+  #blog
+  get '/blog', to: 'blog#index', as: 'blogs'
+  get '/blog/new', to: 'blog#new'
+  post '/blog', to: 'blog#create', as: 'new_blog'
+  get '/blog/:id', to: 'blog#show', as: 'blog'
+  get '/blog/:id/edit', to: 'blog#edit', as: 'edit_blog'
+  patch '/blog/:id', to: 'blog#update'
+  delete '/blog/:id', to: 'blog#destroy', as: 'delete_blog'
 end
